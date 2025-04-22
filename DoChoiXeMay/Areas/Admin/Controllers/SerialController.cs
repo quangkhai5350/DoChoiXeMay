14da3265cfcee6e0bc769862176f67e10eb5a1c9
Tran Quang Khai <tranquangkhaiiq@gmail.com>
2025-04-22 16:21:34 +0700

    chieu22/04

---
+++ DoChoiXeMay/Areas/Admin/Controllers/SerialController.cs
@@ -0,0 +1,32 @@
+﻿using DoChoiXeMay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoChoiXeMay.Areas.Admin.Controllers
+{
+    public class SerialController : Controller
+    {
+        // GET: Admin/Serial
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListSerialChuaIn()
+        {
+            return View();
+        }
+        public ActionResult AddNewSerial()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddNewSerial(Ser_sp SerSP)
+        {
+            return RedirectToAction("ListSerialChuaIn");
+        }
+    }
+}
