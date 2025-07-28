a19154e3b938b9b8edb6481bd1f29d84ee802092
Tran Quang Khai <tranquangkhaiiq@gmail.com>
2025-07-28 16:12:26 +0700

    28thang7

--- DoChoiXeMay/Filters/ProtectNVAttribute.cs
+++ DoChoiXeMay/Filters/ProtectNVAttribute.cs
@@ -11,7 +11,7 @@ namespace DoChoiXeMay.Filters
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             var quyen = HttpContext.Current.Session["quyen"];
-            if (quyen == null || int.Parse(quyen.ToString()) <3 && int.Parse(quyen.ToString()) > 4)
+            if (quyen == null || int.Parse(quyen.ToString())>2 && int.Parse(quyen.ToString()) <5)
             {
                 //HttpContext.Current.Session["Message"] = "Vui lòng đăng nhập";
                 filterContext.HttpContext.Session["ThongbaoLogin"] = "Phiên làm việc đã kết thúc, vui lòng đăng nhập lại.";
